c3c9bb1e1ad4b4b76443f7b53708bd89ccc4958e
Parker Lusk <parkerclusk@gmail.com>
2015-06-20 21:44:05 -0600

    setup jade template

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var app         = express();
 var mongoose    = require('mongoose');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
+var path        = require('path');
+var api         = require('./app/api');
+var routes      = require('./app/routes')
 var database    = require('./config/database');     // database configs
 
 var port        = process.env.PORT || 8080;         // If no env var set, DEV mode
@@ -26,6 +29,9 @@ var port        = process.env.PORT || 8080;         // If no env var set, DEV mo
 
 mongoose.connect(database.url);
 mongoose.connection.once('open', function() { console.log('DB Connected!'); });
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/app/views');
 
 app.use(bodyParser.json());                         // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -41,10 +47,12 @@ app.use(require('./app/middleware/img-upload')());  // Handle the Image uploads
 // Routes
 // ----------------------------------------------------------------------------
 
-app.use(express.static(__dirname + '/public'));                 // for the HTML5 app
+app.use(express.static(path.join(__dirname, 'public')));                // for the HTML5 app
 app.use('/uploads', express.static(__dirname + '/uploads'));    // for imgs
 
-app.use('/api', require('./app/routes'));
+app.use('/api', api);
+
+app.get('/', routes.index);
 
 // ----------------------------------------------------------------------------
 // Listen (start app: `node app.js`)
